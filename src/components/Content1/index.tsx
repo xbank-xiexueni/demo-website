ff1ef7a66b9814e25fbef1e8e5cd0cfab9364bea
xxn-home <xiexueni@xbank.plus>
2023-04-19 23:12:14 +0800

    fix: event listener error

--- src/components/Content1/index.tsx
+++ src/components/Content1/index.tsx
@@ -14,22 +14,28 @@ import scrollJson from '../../constants/Scroll-helper.json';
 export const CONTENT1_HEIGHT = 982;
 const Content1 = () => {
 
-  useLayoutEffect(() => {
-    console.log('typeof document:',typeof document,typeof window === 'undefined','typeof window:',typeof window)
+  useEffect(() => {
+    console.log('1.typeof document:',typeof document,typeof window === 'undefined','typeof window:',typeof window)
     if (typeof window === 'undefined') return;
     const bgx = document.getElementById('bgx');
+    console.log('2. bgx:',bgx)
     if (!bgx) return;
-
     bgx.onmouseenter = (e) => {
       const enterX = e.clientX;
+      console.log('3. enterX:',enterX)
       const enterY = e.clientY;
       if (!bgx) return;
       const cliX = parseInt(bgx.style.backgroundPosition.split(' ')[0]) || 0;
       const cliY = parseInt(bgx.style.backgroundPosition.split(' ')[1]) || 0;
+      console.log('4. cliX:',cliX)
+
       bgx.onmousemove = (e) => {
         const currX = cliX + (e.clientX - enterX) * 0.2;
         const currY = cliY + (e.clientY - enterY) * 0.2;
+        console.log('5. currX:',currX)
+
         if (currX >= -150 && currX <= 0 && !!bgx) {
+          console.log('6. move')
           bgx.style.setProperty('--x', currX + "px");
           bgx.style.setProperty('--y', currY + "px")
         }
@@ -38,8 +44,14 @@ const Content1 = () => {
   }, []);
 
   const minHeight = useMemo(()=> {
+    console.log('1. typeof document:',typeof document,typeof window === 'undefined','typeof window:',typeof window)
+
     if (typeof window === 'undefined') return 982; 
+    console.log('2. typeof document:',typeof document,typeof window === 'undefined','typeof window:',typeof window)
+
     const windowWidth = document.documentElement.clientWidth || document?.body?.clientWidth
+    console.log('3. windowWidth:',windowWidth)
+
     if(!windowWidth) return 982
     return windowWidth * 982 / 1440
   },[])
